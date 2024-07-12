b8becc9d291392c9d78078c2c6a3904b8e7dbba0
Mike Christie <mike99christie@btinternet.com>
2024-07-12 13:24:00 +0100

    simplify option name

--- classes/class-u3a-group.php
+++ classes/class-u3a-group.php
@@ -633,7 +633,7 @@ class U3aGroup
             $button_identifier = "list_button_anchor";
             $button_anchor = "#$button_identifier";
             // if disabled don't use the anchor.
-            if ('disabled' == get_option('u3a_groups_list_scroll_disable', '')) {
+            if ('disabled' == get_option('u3a_groups_list_scroll', 'enabled')) {
                 $button_anchor = '';
             }
             $html = <<<END
